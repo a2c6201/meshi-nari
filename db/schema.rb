98dbb2a5a0e393e1d410245ae6b18f699e9d1e29
a2c6201 <a2c6201@gmail.com>
2024-07-27 19:48:48 +0900

    feat: rails g model menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_15_075057) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_27_093748) do
+  create_table "menus", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
