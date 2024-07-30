7b243d0ea315fd3984ce2c21ad17a866ccc1db88
a2c6201 <a2c6201@gmail.com>
2024-07-30 22:02:12 +0900

    feat: train-nari-api.onrender.comからのリクエストを許可

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  # train-nari-api.onrender.com からのリクエストを許可
+  config.hosts << "train-nari-api.onrender.com"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
