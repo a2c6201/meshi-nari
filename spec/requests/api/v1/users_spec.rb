999311772f04f624f9bc903973935a6d3a1bd6bd
a2c6201 <a2c6201@gmail.com>
2024-08-19 22:52:23 +0900

    test: show user

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -62,35 +62,45 @@ RSpec.describe 'api/v1/users', type: :request do
   path '/api/v1/users/{id}' do
     # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
+    # factory_botでダミーユーザー作成
+    before do
+      @user = FactoryBot.create(:user)
+    end
 
-    # get('show user') do
-    #   response(200, 'successful') do
-    #     let(:id) { '123' }
+    get('show user') do
+      response(200, 'successful') do
+        schema type: :object,
+          properties: {
+            id: { type: :integer },
+            name: { type: :string },
+            email: { type: :string },
+            created_at: { type: :string, format: 'date-time' },
+            updated_at: { type: :string, format: 'date-time' }
+          }
+        let(:id) { @user.id }
 
-    #     after do |example|
-    #       example.metadata[:response][:content] = {
-    #         'application/json' => {
-    #           example: JSON.parse(response.body, symbolize_names: true)
-    #         }
-    #       }
-    #     end
-    #     run_test!
-    #   end
-    # end
+        after do |example|
+          data = JSON.parse(response.body, symbolize_names: true)
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: data
+            }
+          }
+        end
+        run_test!
+      end
+    end
 
-    # factory_botで作成したユーザーを削除する
     delete('delete user') do
       consumes 'application/json'
-      before do
-        @user = FactoryBot.create(:user)
-      end
       response(200, 'successful') do
         let(:id) { @user.id }
 
         after do |example|
+          data = JSON.parse(response.body, symbolize_names: true)
           example.metadata[:response][:content] = {
             'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
+              example: data
             }
           }
         end
